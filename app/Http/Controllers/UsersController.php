6b846f2c89b154aa23b8afa95c46677a8b1ec9e0
caojianx <570704361@qq.com>
2018-10-09 07:49:23 +0000

    user policy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -15,11 +20,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, UserRequest $request,ImageUploadHandler $uploader)
     {
+        $this->authorize('update',$user);
         $data = $request->all();
         if($request->avatar){
             $result = $uploader->save($request->avatar,'avatars',$user->id,362);
@@ -30,4 +37,5 @@ class UsersController extends Controller
         $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
+
 }
