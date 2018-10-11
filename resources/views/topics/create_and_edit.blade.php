79d2261be5319f65eaa837ceb205a0e25dbb290a
caojianx <570704361@qq.com>
2018-10-11 06:47:47 +0000

    user can upload topic image

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -2,60 +2,62 @@
 
 @section('content')
 
-<div class="container">
-    <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
-            
-            <div class="panel-body">
-                <h2 class="text-center">
-                    <i class="glyphicon glyphicon-edit"></i>
-                    @if($topic->id)
-                        编辑话题
-                    @else
-                        新建话题
-                    @endif
-                </h2>
-            </div>
+    <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
 
-            @include('common.error')
-
-            <div class="panel-body">
-                @if($topic->id)
-                    <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
-                        <input type="hidden" name="_method" value="PUT">
-                @else
-                    <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
-                @endif
-
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                    
-                <div class="form-group">
-                	<input class="form-control" type="text" name="title" id="title-field" placeholder="请填写标题" value="{{ old('title', $topic->title ) }}" />
-                </div> 
-
-                <div class="form-group">
-                    <select name="category_id" class="form-control" required>
-                        <option value="" hidden disabled selected>请选择分类</option>
-                        @foreach($categories as $category)
-                            <option value="{{$category->id}}">{{$category->name}}</option>
-                        @endforeach
-                    </select>
+                <div class="panel-body">
+                    <h2 class="text-center">
+                        <i class="glyphicon glyphicon-edit"></i>
+                        @if($topic->id)
+                            编辑话题
+                        @else
+                            新建话题
+                        @endif
+                    </h2>
                 </div>
-                 <div class="form-group">
-                     <textarea name="body" id="editor" class="form-control" rows="3" required placeholder="请填入至少是哪个字符的内容">{{ old('body', $topic->body ) }}</textarea>
-                  </div>
-                    <div class="well well-sm">
-                        <button type="submit" class="btn btn-primary">
-                            <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                            保存
-                        </button>
-                    </div>
-                </form>
+
+                @include('common.error')
+
+                <div class="panel-body">
+                    @if($topic->id)
+                        <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
+                            <input type="hidden" name="_method" value="PUT">
+                            @else
+                                <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
+                                    @endif
+
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+
+                                    <div class="form-group">
+                                        <input class="form-control" type="text" name="title" id="title-field"
+                                               placeholder="请填写标题" value="{{ old('title', $topic->title ) }}"/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <select name="category_id" class="form-control" required>
+                                            <option value="" hidden disabled selected>请选择分类</option>
+                                            @foreach($categories as $category)
+                                                <option value="{{$category->id}}">{{$category->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <textarea name="body" id="editor" class="form-control" rows="3" required
+                                                  placeholder="请填入至少是哪个字符的内容">{{ old('body', $topic->body ) }}</textarea>
+                                    </div>
+                                    <div class="well well-sm">
+                                        <button type="submit" class="btn btn-primary">
+                                            <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                                            保存
+                                        </button>
+                                    </div>
+                                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
 @section('styles')
@@ -69,7 +71,15 @@
     <script>
         $(document).ready(function () {
             var editor = new Simditor({
-                textarea:$('#editor'),
+                textarea: $('#editor'),
+                upload: {
+                    url: '{{route('topics.upload_image')}}',
+                    params: {_token: '{{csrf_token()}}'},
+                    fileKey: 'upload_file',
+                    connectionCount: 3,
+                    leaveConfirm: '文件上传中，关闭此页面将取消上传。',
+                },
+                pasteImage: true,
             });
         });
     </script>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ Auth::routes();
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories','CategoriesController',['only'=>['show']]);
+Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
