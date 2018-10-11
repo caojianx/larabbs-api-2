141890f9d113f8ca82dd04f3660415a1c19351a1
caojianx <570704361@qq.com>
2018-10-11 02:17:37 +0000

    category topic list

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 Route::get('/','PagesController@root')->name('root');
 Auth::routes();
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
-
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories','CategoriesController',['only'=>['show']]);
