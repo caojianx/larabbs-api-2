4aa9ca91b0d715e7e1c25e7a35f191e1bdc45c26
caojianx <570704361@qq.com>
2018-10-22 07:52:55 +0000

    notifications stats

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -9,7 +9,14 @@ class NotificationsController extends Controller
 {
     public function index()
     {
-        $notifications=$this->user()->notifications()->paginate(20);
-        return $this->response->paginator($notifications,new NotificationTransformer());
+        $notifications = $this->user()->notifications()->paginate(20);
+        return $this->response->paginator($notifications, new NotificationTransformer());
+    }
+
+    public function stats()
+    {
+        return $this->response->array([
+           'unread_count'=>$this->user()->notification_count,
+        ]);
     }
 }
